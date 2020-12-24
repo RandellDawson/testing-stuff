7c5eef7a0261ac15709b273a471d558d1104be69
Randell Dawson <rdawson@onepathtech.com>
2020-12-24 13:53:58 -0700

    fix: use pr num as input

--- .github/actions/crowdin-updater/index.js
+++ .github/actions/crowdin-updater/index.js
@@ -1,9 +1,17 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const fetch = require('node-fetch');
 
 try {
   core.info(JSON.stringify(github.context.payload, null, 2));
-  const { pull_request: { number: prNum } } = github.context.payload;
+  const {
+    pull_request: {
+      number: prNum,
+      commits: {
+        href: prCommitsUrl
+      }
+    }
+  } = github.context.payload;
   core.info(`#${prNum} was merged`);
 } catch (error) {
   core.setFailed(error.message);
