8ad44b8cd6530e362f15396d38f8a8a58f5481f7
Randell Dawson <rdawson@onepathtech.com>
2020-12-25 13:22:29 -0700

    fix: creat use commit pushed

--- .github/actions/crowdin-updater/index.js
+++ .github/actions/crowdin-updater/index.js
@@ -1,18 +1,43 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
-const fetch = require('node-fetch');
+const util = require('util');
 
-try {
-  core.info(JSON.stringify(github.context.payload, null, 2));
-  const {
-    pull_request: {
-      number: prNum,
-      commits: {
-        href: prCommitsUrl
+const getOutputFromCommand = async (command) => {
+  try {
+    const { stdout } = await exec(command);
+    return stdout;
+  }
+  catch (err) {
+    console.log('we have an error');
+    console.log('command');
+    console.log(command + '\n');
+    return undefined
+  };
+};
+
+(async () => {
+  try {
+    const commit = core.getInput('commit-sha');
+    core.info(`commit #${commit} was pushed`);
+    const exec = util.promisify(require('child_process').exec);
+    const diffCommand = `git show -m --name-status ${commit}^..${commit}`;
+    const diff = await getOutputFromCommand(diffCommand);
+    core.info(diff);
+    const files = diff.split('\n');
+    if (files && file.length) {
+      for (let file of files) {
+        const [ change, filename ] = file.split(/\s+/);
+        core.info(change + ' - ' + filename);
       }
     }
-  } = github.context.payload;
-  core.info(`#${prNum} was merged`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+    process.exit();
+    let prevCommitCommand = `git show ${commit}^:${filepath}`;
+    let currCommitCommand = `git show ${commit}:${filepath}`;
+
+    const oldContent = await getOutputFromCommand(prevCommitCommand);
+    const newContent = await getOutputFromCommand(currCommitCommand);
+    core.info(oldContent + '\n************\n' + newContent);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+
+})();
