c08c75e96dca0d45827414243b57c3b5be0c142e
Randell Dawson <rdawson@onepathtech.com>
2020-12-26 12:31:44 -0700

    fix: changed certificaton

--- .github/actions/crowdin-updater/index.js
+++ .github/actions/crowdin-updater/index.js
@@ -3,15 +3,17 @@ const { getOutputFromCommand, crowdinMethods: crowdin } = require('./utils');
 const learnDir = '';
 // incorporate the following into diffCommand once sure everything works
 // const learnDir = ' -- curriculum/challenges/english';
-const learnRegex = /^curriculum\/challenges\/english\/\d\d-(?!certifications)/;
+const learnRegex = /^curriculum\/challenges\/english\/\d\d-(?!certificates)/;
 
 (async () => {
   try {
     const commit = core.getInput('commit-sha');
     core.info(`commit # pushed: ${commit}`);
 
-    const logCommand = `git log -n 10 --pretty=format:'%h -%d %s (%cr) <%an>' --abbrev-commit > log.txt`;
+    const logCommand = `git log -n 5 --pretty=format:'%h -%d %s (%cr) <%an>' --abbrev-commit > log.txt`;
     const log = await getOutputFromCommand(logCommand);
+    console.log('log below');
+    console.log(log);
 
     const diffCommand = `git diff --name-status -m -M -C ${commit}^..${commit}${learnDir}`;
     const diff = await getOutputFromCommand(diffCommand);
@@ -23,22 +25,21 @@ const learnRegex = /^curriculum\/challenges\/english\/\d\d-(?!certifications)/;
       const learnFiles = files
         .map(file => {
           const [change, origFilename, newFilename] = file.split(/\s+/);
-          let fileObj = { change, origFilename };
+          // only need about first letter of change status code
+          let fileObj = { change: change[0], origFilename };
           if (newFilename) {
             fileObj = { ...fileObj, newFilename };
           }
           return fileObj;
         })
         .filter(({ change, origFilename, newFilename }) => {
-          // only need about first letter of change status code
-          const code = change[0];
           if (
-            (code === 'A' || code === 'M' || code === 'D') &&
+            (change === 'A' || change === 'M' || change === 'D') &&
             origFilename.match(learnRegex)
           ) {
             return true;
           } else if (
-            (code === 'R' || code === 'C') &&
+            (change === 'R' || change === 'C') &&
             (origFilename.match(learnRegex) || newFilename.match(learnRegex))
           ) {
             return true;
@@ -50,9 +51,9 @@ const learnRegex = /^curriculum\/challenges\/english\/\d\d-(?!certifications)/;
           ? 'learn curriculum in commit\n' + JSON.stringify(learnFiles, null, 2)
           : 'no learn files in commit'
       );
+      return learnFiles;
     }
     core.info('action complete');
-    process.exit();
   } catch (error) {
     core.setFailed(error.message);
   }
